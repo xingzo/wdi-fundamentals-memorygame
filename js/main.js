bb25ace1373ca12fbddab606b93d2fb3a8b1d852
Kingsley <kingsleynyaosi@gmail.com>
2018-01-15 05:46:07 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
-var cards = ["queen", "queen", "king", "king"];
 
-//array to hold the cards that are in play
+//arrays to hold the cards
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-//the first card flipped
-var cardOne = cards[3];
-cardsInPlay.push(cardOne);
-
-//the seond card flipped
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if( cardsInPlay.length == 2)
+var checkForMatch = function()
 {
-  if (cardOne == cardTwo)
-    alert("You found a match!");
+  if(cardsInPlay[0]==cardsInPlay[1])
+    console.log("You found a match!");
   else
-    alert("Sorry, try again.");
-}
+    console.log("Sorry, try again.");
 
-console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardTwo);
+
+
+};
+
+var flipCard = function(cardId)
+{
+  console.log("User flipped " + cards[cardId]);
+
+  //here we add the card flipped to the cards in play
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+
+
+  //if( cardsInPlay.length == 2)
+  //{
+  //  if (cardOne == cardTwo)
+  //    alert("You found a match!");
+  //  else
+  //    alert("Sorry, try again.");
+  //}
+
+};
+
+flipCard(0);
+flipCard(2);
+
+
+//the first card flipped
+
+//
+//console.log("User flipped " + cardOne);
+
+//console.log("User flipped " + cardTwo);
