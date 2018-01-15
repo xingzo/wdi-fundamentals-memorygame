de1ffce49e8280c18399e56922e61d3c3ca08f4a
Kingsley <kingsleynyaosi@gmail.com>
2018-01-14 18:19:50 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-console.log ("I'm Kenyan and Proud");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//array to hold the cards that are in play
+var cardsInPlay = [];
 
+//the first card flipped
+var cardOne = cards[3];
+cardsInPlay.push(cardOne);
+
+//the seond card flipped
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if( cardsInPlay.length == 2)
+{
+  if (cardOne == cardTwo)
+    alert("You found a match!");
+  else
+    alert("Sorry, try again.");
+}
 
 console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardTwo);
