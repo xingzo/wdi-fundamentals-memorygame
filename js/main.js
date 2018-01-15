72f086a560acd02eb4d70d29324d608690acfc4c
Kingsley <kingsleynyaosi@gmail.com>
2018-01-15 11:26:42 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,36 +1,50 @@
 
 //arrays to hold the cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+      rank: "king",
+      suit: "hearts",
+      cardImage: "images/king-of-hearts.png"
+
+  },
+  {
+      rank: "king",
+      suit: "diamonds",
+      cardImage: "images/king-of-diamonds.png"
+  }
+
+];
 var cardsInPlay = [];
 
 var checkForMatch = function()
 {
   if(cardsInPlay[0]==cardsInPlay[1])
-    console.log("You found a match!");
+    alert("You found a match!");
   else
-    console.log("Sorry, try again.");
-
-
-
+    alert("Sorry, try again.");
 
 };
 
 var flipCard = function(cardId)
 {
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].cardImage);
+
 
   //here we add the card flipped to the cards in play
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
-
-
-  //if( cardsInPlay.length == 2)
-  //{
-  //  if (cardOne == cardTwo)
-  //    alert("You found a match!");
-  //  else
-  //    alert("Sorry, try again.");
-  //}
 
 };
 
